2dea74ed15740cfcadcb80d523cddc57710d05a9
VEX_ADNAN-404 <166968155+MR-AYAN-CHOWDHURY@users.noreply.github.com>
2025-01-22 23:46:11 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🦋𝗩𝗘𝗫_𝗔𝗗𝗡𝗔𝗡🦋";
+const doNotDelete = "🦋AZIM HUNTER🦋";
 /** 
 * @author NTKhang
 * @author: do not delete it
